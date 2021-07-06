cae7a02a2916b15622a237b29883648a2840355f
galaxy126 <galaxy85126@gmail.com>
2021-07-07 00:38:13 +0800

    a

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ const isProduction = process.env.NODE_ENV !== 'development';
 import * as http from 'http';
 import * as https from 'https';
 import * as fs from 'fs';
+import * as tls from 'tls';
+
+/* const tls = require('tls'); */
 import * as path from 'path';
 import * as express from 'express';
 
@@ -31,23 +34,63 @@ class WebApp {
 	start() { 
 		const app = express()
 		const server = http.createServer(app);
-		const cert = fs.readFileSync(__dirname+'/../certs/cert.crt');
-		const caBundle:any = fs.readFileSync(__dirname+'/../certs/cert.ca-bundle',{encoding:'utf8'});
-		const ca = caBundle.split('-----END CERTIFICATE-----\r\n') .map((cert:any) => cert +'-----END CERTIFICATE-----\r\n');
-		ca.pop();
-		const key = fs.readFileSync(__dirname+'/../certs/cert.key');
-		let options = {
-			cert: cert,
-			ca: ca,
-			key: key 
-		};
-		const httpsServer = https.createServer(options,app);
+		
+		let appDomainKey = null, appDomainPem = null;
+		if (!isProduction) {
+			appDomainKey = __dirname+'/certs/'+process.env.DOMAIN+'.key';
+			appDomainPem = __dirname+'/certs/'+process.env.DOMAIN+'.pem';
+		} else {
+			appDomainKey = '/etc/letsencrypt/live/bitotc.me/privkey.pem';
+			appDomainPem = '/etc/letsencrypt/live/bitotc.me/fullchain.pem';
+		}
+		const appKey = fs.existsSync(appDomainKey) && fs.readFileSync(appDomainKey).toString();
+		const appPem = fs.existsSync(appDomainPem) && fs.readFileSync(appDomainPem).toString();
+		const contextApp = (appKey && appPem) && tls.createSecureContext({key: appKey,cert: appPem});
+		const httpsServer = https.createServer({
+			SNICallback: function(domain, cb) {
+				cb(null, contextApp);
+			},
+			key: appKey,
+			cert: appPem
+		}, app);
+		/* 
+		if (fs.existsSync(__dirname+'/../certs/cert.crt')) {
+			const cert = fs.readFileSync(__dirname+'/../certs/cert.crt');
+			const caBundle:any = fs.readFileSync(__dirname+'/../certs/cert.ca-bundle',{encoding:'utf8'});
+			const ca = caBundle.split('-----END CERTIFICATE-----\r\n') .map((cert:any) => cert +'-----END CERTIFICATE-----\r\n');
+			ca.pop();
+			const key = fs.readFileSync(__dirname+'/../certs/cert.key');
+			let options = {
+				cert: cert,
+				ca: ca,
+				key: key 
+			};
+			httpsServer = https.createServer(options,app);
+		} */
 
 		app.use(cors());
 		
 		const FRONTENDPATH = path.normalize(__dirname + '/../../frontend/build');
 		app.use(express.static(FRONTENDPATH));
 		app.use(bodyParser.json());
+		app.use((req, res) => {
+			let hostname = req.headers.host;
+			if(req.protocol==='http') {
+				if(!req.url || req.url.indexOf('/.well-known/acme-challenge')===-1) {
+					return res.redirect('https://' + hostname + (req.url||''));
+				} else {
+					const acmeFile = __dirname + '/../certs/.dnskey';
+					if (fs.existsSync(acmeFile)) {
+						const acme = fs.readFileSync(__dirname + '/../certs/.dnskey').toString();
+						res.send(acme);
+					} else {
+						res.send('no found');
+					}
+					return;
+				}
+			}
+			res.setHeader("Content-Security-Policy","script-src-elem 'self' https://"+hostname+' https://cdnjs.cloudflare.com');
+		});
 		app.use(routes);
 		
 		app.get('*', (req,res) =>{
